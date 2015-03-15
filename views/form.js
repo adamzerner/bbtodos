75781de7aded1aa0088317d606261c2669421174
Adam Zerner <azerner3@gmail.com>
2015-03-15 11:52:50 -0400

    create

--- views/form.js
+++ views/form.js
@@ -9,5 +9,29 @@ app.FormView = Backbone.View.extend({
     var obj = {};
     obj.type = 'Create';
     this.$el.html( this.template(obj) );
+  },
+
+  events: {
+    'submit form': 'submit'
+  },
+
+  submit: function(e) {
+    e.preventDefault();
+    var type = $(e.target).data('type');
+    if (type === 'Create') {
+      app.todos.create({
+        text: $('input').val()
+      }, {
+        success: function() {
+          app.router.navigate('list', {trigger: true});
+        },
+        error: function() {
+          alert('Unable to create todo');
+        }
+      });
+    }
+    else if (type === 'Update') {
+
+    }
   }
 });
