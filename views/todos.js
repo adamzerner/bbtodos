b41437a77f9a5acb57efda14f224c61406d1f660
Adam Zerner <azerner3@gmail.com>
2015-03-15 11:59:35 -0400

    list

--- views/todos.js
+++ views/todos.js
@@ -4,6 +4,11 @@ app.TodosView = Backbone.View.extend({
   el: $('#main'),
 
   render: function() {
+    var self = this;
     this.$el.html( $('#todos-template').html() );
+    app.todos.forEach(function(todoModelInstance) {
+      var todoViewInstance = new app.TodoView({model: todoModelInstance});
+      self.$el.find('ul').append(todoViewInstance.render().el);
+    });
   }
 });
