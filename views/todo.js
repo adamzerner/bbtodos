ebf85cb7326d4a74796fbb76ba68bac8e2705bb9
Adam Zerner <azerner3@gmail.com>
2015-03-15 12:22:07 -0400

    delete

--- views/todo.js
+++ views/todo.js
@@ -8,5 +8,25 @@ app.TodoView = Backbone.View.extend({
   render: function() {
     this.$el.html( this.template(this.model) );
     return this;
+  },
+
+  events: {
+    'click #delete-todo': 'delete'
+  },
+
+  delete: function(e) {
+    e.preventDefault();
+    $el = $(e.target);
+    var id = $el.data('id');
+    var model = app.todos.get(id);
+    model.destroy({
+      success: function(model) {
+        app.todos.remove(model);
+        $el.closest('li').remove();
+      },
+      error: function() {
+        alert('Unable to delete todo');
+      }
+    })
   }
 });
